6e90300d0f8ba57c8f4bd889aff80050196e8483
Nick Hofstetter <nickhofstetter99@gmail.com>
2024-06-12 07:21:29 +0200

    changed validation in config file

--- projects/CO-DETR/configs/codino/full_new_custom_co_dino_5scale_swin_l_16xb1_1x_coco.py
+++ projects/CO-DETR/configs/codino/full_new_custom_co_dino_5scale_swin_l_16xb1_1x_coco.py
@@ -732,9 +732,9 @@ val_dataloader = dict(
     batch_size=1,
     dataset=dict(
         _scope_='mmdet',
-        ann_file='/opt/ml/input/data/validation/valdalidation_annotations.json', #/opt/ml/input/data/validation/correct_val.json
+        ann_file='/opt/ml/input/data/val/valdalidation_annotations.json', #/opt/ml/input/data/validation/correct_val.json
         backend_args=None,
-        data_prefix=dict(img='/opt/ml/input/data/validation/images/'), #/opt/ml/input/data/validation/images/
+        data_prefix=dict(img='/opt/ml/input/data/val/images/'), #/opt/ml/input/data/validation/images/
         data_root='/opt/ml/input/data/',
         pipeline=[
             dict(backend_args=None, type='LoadImageFromFile'),
@@ -761,7 +761,7 @@ val_dataloader = dict(
     sampler=dict(_scope_='mmdet', shuffle=False, type='DefaultSampler'))
 val_evaluator = dict(
     _scope_='mmdet',
-    ann_file='/opt/ml/input/data/validation/validation_annotations.json', #/opt/ml/input/data/validation/correct_val.json
+    ann_file='/opt/ml/input/data/val/validation_annotations.json', #/opt/ml/input/data/validation/correct_val.json
     backend_args=None,
     format_only=False,
     metric='bbox',
